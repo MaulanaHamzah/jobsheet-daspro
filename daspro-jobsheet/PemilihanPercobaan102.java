e9808f6288de3d1f1d335595632efcd8bc5e5367
MaulanaHamzah <144118963+MaulanaHamzah@users.noreply.github.com>
2023-09-27 08:01:47 +0700

    Update PemilihanPercobaan102.java

--- daspro-jobsheet/PemilihanPercobaan102.java
+++ daspro-jobsheet/PemilihanPercobaan102.java
@@ -4,12 +4,11 @@ public class PemilihanPercobaan102 {
         Scanner input02 = new Scanner(System.in);
 
         System.out.print("Masukkan angka: ");
+        String hasil;
         int angka = input02.nextInt();
 
-        if (angka % 2 == 0)
-            System.out.println("Angka "+angka+" bilangan genap");
-        else
-            System.out.println("Angka "+angka+" bilangan ganjil");
+        hasil = (angka % 2 == 0) ? "Bilangan genap" : "Bilangan ganjil";
+        System.out.println(angka + " adalah " + hasil);
 
     }
 }
