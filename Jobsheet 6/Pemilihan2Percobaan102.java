e612b19eb5bbdf74e34b2e3df3ea16c0ae29d854
MaulanaHamzah <144118963+MaulanaHamzah@users.noreply.github.com>
2023-10-03 18:44:20 +0700

    Update Pemilihan2Percobaan102.java

--- Jobsheet 6/Pemilihan2Percobaan102.java
+++ Jobsheet 6/Pemilihan2Percobaan102.java
@@ -8,14 +8,30 @@ public class Pemilihan2Percobaan102 {
         System.out.print("Masukkan tahun = ");
         tahun = input02.nextInt();
 
+        import java.util.Scanner;
+
+public class Pemilihan2Percobaan102 {
+    public static void main(String[] args) {
+        Scanner input02 = new Scanner(System.in);
+
+        int tahun;
+        System.out.print("Masukkan tahun = ");
+        tahun = input02.nextInt();
+
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0 || (tahun % 400) == 0) {
-                System.out.println("Tahun kabisat");
+            if ((tahun % 100) == 0) {
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun kabisat");
+                } else {
+                    System.out.println("Bukan tahun kabisat");
+                }
             } else {
-                System.out.println("Bukan tahun kabisat");
+                System.out.println("Tahun kabisat");
             }
         } else {
             System.out.println("Bukan keduanya");
         }
+    }
+}
     }
 }
