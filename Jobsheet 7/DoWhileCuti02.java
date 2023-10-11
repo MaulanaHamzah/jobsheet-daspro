c36b8b46178298d5b4ee1b4518fa9dafe3a799a9
MaulanaHamzah <144118963+MaulanaHamzah@users.noreply.github.com>
2023-10-11 11:16:57 +0700

    Update DoWhileCuti02.java

--- Jobsheet 7/DoWhileCuti02.java
+++ Jobsheet 7/DoWhileCuti02.java
@@ -2,25 +2,32 @@ import java.util.Scanner;
 
 public class DoWhileCuti02 {
     public static void main(String[] args) {
-        int jatahCuti = 12;
-        int sisaCuti = jatahCuti;
+        Scanner scan = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
 
-        Scanner input = new Scanner(System.in);
+        System.out.print("Jatah cuti = ");
+        jatahCuti = scan.nextInt();
 
         do {
-            System.out.println("Jumlah sisa cuti: " + sisaCuti + " hari");
-            System.out.print("Masukkan jumlah hari cuti yang ingin diambil: ");
-            int jumlahCuti = input.nextInt();
+            System.out.print("Apakah Anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = scan.next();
 
-            if (jumlahCuti <= sisaCuti) {
-                sisaCuti -= jumlahCuti;
-                System.out.println("Cuti berhasil diambil. Sisa cuti: " + sisaCuti + " hari");
-            } else {
-                System.out.println("Jumlah cuti yang diminta melebihi sisa cuti yang tersedia.");
-                System.out.println("Silakan masukkan jumlah cuti yang sesuai dengan sisa cuti yang tersedia.");
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah hari = ");
+                jumlahHari = scan.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti = " + jatahCuti);
+                } else {
+                    System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                    break;
+                }
+            } else if (konfirmasi.equalsIgnoreCase("t")) {
+                System.out.println("Terima kasih telah menggunakan program ini");
+                break;
             }
-        } while (sisaCuti > 0);
-
-        System.out.println("Jatah cuti tahunan telah habis.");
+        } while (jatahCuti > 0);
     }
 }
