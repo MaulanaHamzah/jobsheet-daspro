ea8fe42602b5e2c7baff0766d53d54e395164971
MaulanaHamzah <144118963+MaulanaHamzah@users.noreply.github.com>
2023-10-11 07:46:56 +0700

    Update WhileGaji02.java

--- Jobsheet 7/WhileGaji02.java
+++ Jobsheet 7/WhileGaji02.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 public class WhileGaji02 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int jumlahKaryawan, jumlahJamLembur;
         double gajiLembur = 0, totalGajiLembur = 0;
         String jabatan;
         System.out.print("Masukkan jumlah karyawan = ");
-        jumlahKaryawan = scan.nextInt();
+        jumlahKaryawan = sc.nextInt();
         int i = 0;
         while (i < jumlahKaryawan) {
             System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
             System.out.print("Masukkan jabatan karyawan ke- " + (i+1) + ": ");
-            jabatan = scan.next();
+            jabatan = sc.next();
             System.out.print("Masukkan jumlah jam lembur = ");
-            jumlahJamLembur = scan.nextInt();
+            jumlahJamLembur = sc.nextInt();
             i++;
             if (jabatan.equalsIgnoreCase("direktur")) {
                 continue;
