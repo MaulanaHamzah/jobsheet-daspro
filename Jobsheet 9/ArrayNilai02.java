4089a7d4bebfc02109bdeab48a52a579a6a1c069
MaulanaHamzah <144118963+MaulanaHamzah@users.noreply.github.com>
2023-10-25 07:33:49 +0700

    Update ArrayNilai02.java

--- Jobsheet 9/ArrayNilai02.java
+++ Jobsheet 9/ArrayNilai02.java
@@ -4,14 +4,20 @@ public class ArrayNilai02 {
         Scanner input = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = input.nextInt();
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus! ");
+            } 
+            
+            else if (nilaiAkhir[i] < 70) {
+                System.out.println("Mahasiswa ke-"+i+" tidak lulus! ");
+            }
         }
 
     }
-} 
+}
