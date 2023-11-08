cfc025179529f8d491c806852802ba7f5e0d3025
MaulanaHamzah <144118963+MaulanaHamzah@users.noreply.github.com>
2023-11-08 09:50:49 +0700

    Update Star.java

--- Jobsheet 11/Star.java
+++ Jobsheet 11/Star.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 public class Star {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
-         System.out.println("");
+         System.out.println("*");
     }
 }
